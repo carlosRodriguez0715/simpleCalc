0037dd5dcad4c5cd3d925782228e9c482c82f47a
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-04-11 10:44:08 -0400

    Initial base structure

---
+++ calculator/src/controllers/CalculatorController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
+public class CalculatorController {
+	
+	@FXML private AnchorPane basePane;
+	@FXML private Button btn9, btn8, btn7, btn6, btn5, btn4, btn3, btn2, btn1, btn0, eq, dot, div, mul, sub, add;
+	@FXML private TextArea txtArea;
+	
+	@FXML private void initialize() {
+		
+	}
+
+}
